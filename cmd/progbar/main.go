4a766a94e7e363d7be70b887e70c1101dab2646c
ry023 <ry023.blue@gmail.com>
2025-07-02 09:33:38 +0900

    2 commands

--- cmd/progbar/main.go
+++ cmd/progbar/main.go
@@ -7,27 +7,51 @@ import (
 )
 
 func main() {
-	log.Print("Starting slide processing...")
-	s, err := parse()
-	if err != nil {
-		panic(err)
+	arg := os.Args
+	if len(arg) != 2 || (arg[1] != "insert" && arg[1] != "remove") {
+		fmt.Println("Usage: progbar [insert|remove]")
+		os.Exit(1)
 	}
-	log.Println("Complete.")
 
+	cmd := arg[1]
+
+	if cmd == "remove" {
+		remove()
+	} else if cmd == "insert" {
+		insert()
+	}
+}
+
+func remove() {
 	log.Print("Deleting existing progress bars...")
 	if err := deleteProgressBars(); err != nil {
 		panic(err)
 	}
 	log.Println("Complete.")
+}
 
-	log.Print("Inserting progress bars...")
-	if err := os.RemoveAll("tmp"); err != nil {
-		panic(fmt.Sprintf("Failed to remove tmp directory: %v", err))
-	}
-	for _, index := range s.enabledIndexes {
-		if err := insertProgressBar(index, s); err != nil {
-			panic(fmt.Sprintf("Failed to insert progress bar for slide %d: %v", index, err))
+func insert() {
+		log.Print("Parsing slides...")
+		s, err := parse()
+		if err != nil {
+			panic(err)
 		}
-	}
-	log.Println("Complete.")
+		log.Println("Complete.")
+
+		log.Print("Deleting existing progress bars...")
+		if err := deleteProgressBars(); err != nil {
+			panic(err)
+		}
+		log.Println("Complete.")
+
+		log.Print("Inserting progress bars...")
+		if err := os.RemoveAll("tmp"); err != nil {
+			panic(fmt.Sprintf("Failed to remove tmp directory: %v", err))
+		}
+		for _, index := range s.enabledIndexes {
+			if err := insertProgressBar(index, s); err != nil {
+				panic(fmt.Sprintf("Failed to insert progress bar for slide %d: %v", index, err))
+			}
+		}
+		log.Println("Complete.")
 }
